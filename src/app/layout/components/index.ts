7d8f6516125aad97cf732862ea19723c0391cb0d
davidtuan02 <tuantienti050222@gmail.com>
2025-05-17 15:19:00 +0700

    add footer

--- src/app/layout/components/index.ts
+++ src/app/layout/components/index.ts
@@ -2,10 +2,12 @@ import { TopbarComponent } from './topbar/topbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SidebarResizerComponent } from './sidebar-resizer/sidebar-resizer.component';
+import { FooterComponent } from './footer/footer.component';
 
 export {
   TopbarComponent,
   SidebarComponent,
   LayoutComponent,
   SidebarResizerComponent,
+  FooterComponent,
 }
--- src/app/layout/components/layout/layout.component.ts
+++ src/app/layout/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SidebarComponent, TopbarComponent } from '../../../layout/components';
 import { RouterOutlet } from '@angular/router';
 import { SvgDefinitionsComponent } from '../../../shared/components';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, TopbarComponent, SvgDefinitionsComponent],
+  imports: [RouterOutlet, SidebarComponent, TopbarComponent, SvgDefinitionsComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
