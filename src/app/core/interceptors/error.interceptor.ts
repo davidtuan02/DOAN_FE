43eab6d5a2554c1179761857a3a916aa956c19aa
davidtuan02 <tuantienti050222@gmail.com>
2025-05-18 19:25:39 +0700

    update

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -4,18 +4,34 @@ import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest,
+  HttpErrorResponse,
 } from "@angular/common/http";
+import * as fromStore from '../../core/store';
 import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
 
 @Injectable({ providedIn: "root" })
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private readonly router: Router,
+    private store: Store<fromStore.AppState>,
+  ) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return next
-      .handle(req)
-      .pipe(catchError((err) => throwError(() => err.error)));
+    return next.handle(req).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.store.dispatch(fromStore.logout());
+          this.router.navigate(['/login']);
+        }
+
+        return throwError(() => err.error);
+      })
+    );
   }
 }
